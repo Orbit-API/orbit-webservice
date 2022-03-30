9a0541d48a31ac307870c7d3d2d39e9af68da488
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2022-03-30 12:21:31 -0300

    Project creation using Spring Initializr

---
+++ orbit-webservice/src/test/java/com/api/orbitwebservice/OrbitWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.orbitwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
