514baa022470c63c9b7c4b8a9ae2c82bff866434
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2022-04-06 07:34:46 -0300

    Adding Cors to Security Configuration

---
+++ orbit-webservice/src/main/java/com/api/orbitwebservice/security/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.api.orbitwebservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+public class SecurityConfiguration {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
