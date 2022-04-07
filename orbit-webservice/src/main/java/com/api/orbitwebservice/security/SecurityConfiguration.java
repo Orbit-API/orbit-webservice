e23bd40149cbca29f4f5766da05f37a42e3b60eb
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2022-04-07 07:47:26 -0300

    Cors setting fix

--- orbit-webservice/src/main/java/com/api/orbitwebservice/security/SecurityConfiguration.java
+++ orbit-webservice/src/main/java/com/api/orbitwebservice/security/SecurityConfiguration.java
@@ -1,11 +1,23 @@
 package com.api.orbitwebservice.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll();
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
